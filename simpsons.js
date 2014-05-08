f3affa26cc7174a962c2a14a16d1774da0d2fa8c
Carsten Lamm <CarstenLamm@googlemail.com>
2014-05-08 16:07:11 +0200

    adds files to work on

---
+++ simpsons.js
@@ -0,0 +1,28 @@
+function Talk(person, sentence) {          
+  	 if 	(person = Maggie) return person + ' can\'t talk.';    
+  if   	(person = Bart) return person +  " eat my shorts.";    
+  	 if 	 (sentence = 'lol') return person+' laughs.';  
+  else return person + ' says: ' + sentence;      
+}
+var people	= 	['Bart', "Homer", 'Lisa', 'Maggie', 'Marge'];     
+ 	  		  
+function greet (group) { 	  	 
+  	for (person in group) {
+	   	talk(person, 'hi.')     
+    }   
+};    
+
+var laugh = function(group) {
+
+  for (person in group) {    
+ 	Talk(person, "lol")
+  }      
+};     
+
+     
+var joke = function(person) {       
+	Talk(person,  	'What is Bruce Lee\'s favorite drink? Wataaaaah!')
+};      
+
+greet(people);	joke(people[1]);    
+laugh(people));
